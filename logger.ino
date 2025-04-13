565f4b10d11ab787fb4d6bbb997feaaacf1d2a70
JJJimbo1 <88861660+JJJimbo1@users.noreply.github.com>
2025-04-13 16:03:11 -0500

    Update logger.ino

--- logger.ino
+++ logger.ino
@@ -3,6 +3,7 @@ CS:   Pin 10
 SCK:  Pin 13
 MOSI: Pin 11
 MISO: Pin 12
+5 Volts
 */
 
 #include <SPI.h>
@@ -11,12 +12,28 @@ MISO: Pin 12
 const int sd_chipSelect = 10;
 
 void setup() {
+  Serial.begin(9600);
+  while (!Serial);
   SD.begin(sd_chipSelect);
+  // Uncomment to reset the log
+  // reset_log();
 }
 
-void write_log(float gforce_magnitude) {
-  String text = String(gforce_magnitude, 3);
+void loop() {
+  write_log();
+  read_log();
+  delay(1000);
+}
+
+void write_log() {
   File dataFile = SD.open("datalog.txt");
+  if (!dataFile) { Serial.println("write_log: Error opening datalog.txt"); return; }
+
+  String text = String(dataFile.parseInt() + 1);
+
+  dataFile.close();
+
+  dataFile = SD.open("datalog.txt", FILE_WRITE | O_TRUNC);
 
   if (dataFile) {
     dataFile.println(text);
@@ -26,14 +43,19 @@ void write_log(float gforce_magnitude) {
 }
 
 void read_log() {
-  if (!Serial) {
-    return;
-  }
+  if (!Serial) { return; }
   File dataFile = SD.open("datalog.txt");
+  if (!dataFile) { Serial.println("read_log: Error opening datalog.txt"); return; }
 
-  if (dataFile) {
-    Serial.write(dataFile.read());
-  }
+  Serial.print("Dangerous Driving Events: ");
+  Serial.println(dataFile.parseInt());
 
   dataFile.close();
+}
+
+void reset_log() {
+  if (SD.exists("datalog.txt")) {
+    SD.remove("datalog.txt");
+  }
+  SD.open("datalog.txt", FILE_WRITE).close();
 }
